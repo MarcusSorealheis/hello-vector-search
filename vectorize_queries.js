c5f45a5dbd332c818135f7dc122f043314d45bc4
marcussorealheis <marcuseagan@gmail.com>
2022-10-05 20:20:17 -0700

    fixing the functions.

--- vectorize_queries.js
+++ vectorize_queries.js
@@ -1,52 +1,42 @@
-/* 
-  function to vectorize queries 
-
-  This function needs to created in Atlas. 
-  To get started, create a new App Service and select 
-  "Functions" from the left-hand menu. Click 
-  "Add Function" and choose "Incoming Webhook".
-  Name your function and select a language. 
-  For our purposes, we'll use Node.js.
-  Copy and paste the code below into the code editor.
-
-*/
-exports = async function (query = "all") {
-  const encodedText = encodeURIComponent(query);
-  const conn = context.services
-    .get("mongodb-atlas")
-    .db("sample_mflix")
-    .collection("movies");
-  var response = await context.http
-    .post({
-      url: "https://scalethebrain.com/rest_vector",
-      body: {
-        field_to_vectorize: body,
-      },
-      encodeBodyAsJSON: true,
-    })
-    .then((response) => {
-      const ejson_body = EJSON.parse(response.body.text());
-      return ejson_body;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-
-  /* store query vector and build the aggregation */
-  query_vector = response.vector;
-  const searchAggregation = [
-    {
-      $search: {
-        knn: {
-          path: "plot_vector",
-          query: query_vector,
-          k: 5,
+// This function is the endpoint's request handler.
+// exports = async function({ query, headers, body}, response) {
+  exports = async function ({ headers, body}, response) {
+    const conn = context.services
+      .get("mongodb-atlas")
+      .db("sample_mflix")
+      .collection("movies");
+    var vectorAPIResponse = await context.http
+      .post({
+        url: "https://scalethebrain.com/rest_vector",
+        body: {
+          field_to_vectorize: body,
         },
-      },
-    },
-  ];
-  const searchResults = await conn.aggreregate(searchAggregation).toArray();
-
-  /* return the search results */
-  return searchResults;
-};
+        encodeBodyAsJSON: true,
+      })
+      .then((response) => {
+        const ejson_body = EJSON.parse(response.body.text());
+        return ejson_body;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  
+    /* store query vector and build the aggregation */
+  
+    query_vector = vectorAPIResponse.vector;
+    const searchAggregation = [
+      {
+        $search: {
+          knnBeta: {
+            path: "plot_vectors",
+            vector: query_vector,
+            k: 5
+          },
+        },
+      }
+    ];
+    const searchResults = await conn.aggregate(searchAggregation).toArray();
+  
+    /* return the search results */
+    return searchResults;
+  };
